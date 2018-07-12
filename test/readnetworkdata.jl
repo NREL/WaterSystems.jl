e8dff0f976dee85ee551183d0c0529b22c4f88ed
amason <amanda.mason@NREL.gov>
2018-07-12 11:59:08 -0600

    change test readnetworkdata

--- test/readnetworkdata.jl
+++ test/readnetworkdata.jl
@@ -1,7 +1,7 @@
 if is_linux() || is_apple()
     folder = readdir("test/data/epanetfiles")
     files = [joinpath("data/epanetfiles", folder[i]) for i =1:length(folder)]
-    #include("data/test_system.jl")
+    include("data/test_system.jl")
 end
 
 if is_windows()
