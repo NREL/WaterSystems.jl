b563cffc074c6c969ab12329d96ee40b5d8a1a31
Barrows <clayton.barrows@nrel.gov>
2019-07-16 11:10:26 -0600

    test bug

--- test/constructors.jl
+++ test/constructors.jl
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 pump  = WaterSystems.ConstSpeedPump()
 tank = WaterSystems.RoundTank()
 reservoir = WaterSystems.Reservoir()
@@ -8,15 +7,5 @@ pipes = WaterSystems.RegularPipe()
 valves = WaterSystems.PressureReducingValve()
 demand = WaterSystems.WaterDemand()
 network = WaterSystems.Network()
-=======
-pump  = WaterSystems.ConstSpeedPump
-tank = WaterSystems.RoundTank
-reservoir = WaterSystems.SourceReservoir
-junction = WaterSystems.Junction
-pipes = WaterSystems.RegularPipe
-valves = WaterSystems.PressureReducingValve
-demand = WaterSystems.WaterDemand
-network = WaterSystems.Network
->>>>>>> update-build
 
 true
